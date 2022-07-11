18383ee24df9338f99f99d414dae6e764281e3ec
APETCHO SYLVESTRE <59835968+Sylvestrekafu@users.noreply.github.com>
2022-07-11 15:40:54 +0200

    Update liste8.py

--- Liste/liste8.py
+++ Liste/liste8.py
@@ -1,4 +1,4 @@
-# liste des   valeurs de 0 à 50 avec un pas de 5 entre chaque valeur
+
 liste1 = [x for x in range(0,50,5)]
 liste2 =[x for x in range(0,50,10)]
 print("Eléménts de la iste avec pas 5: ", liste1) # liste des   valeurs de 0 à 50 avec un pas de 5 entre chaque valeur
